d5d376451c3c8c8e9aa7a94f5fb76704600e5be3
Justin Ji <166482436+JiJiaqi01@users.noreply.github.com>
2024-04-17 11:42:11 +0800

    Add files via upload

--- openai-test.py
+++ openai-test.py
@@ -1,5 +1,13 @@
 from openai import OpenAI
-client = OpenAI()
+
+client = OpenAI(
+    base_url = 'https://api.chatanywhere.com.cn/v1',
+    api_key = 'sk-AUnJk0FMih4MJp32GAyNzEQRi5KtVeiZlvRU6tROmaOAFvD9',
+    # http_client=httpx.Client(
+    #   base_url= config.OpenAI.host,
+    #   follow_redirects = True,
+    # ),
+)
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
