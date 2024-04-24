7ef7ed78a7651de7266b2c5ba5f9707a3d72b2a0
typist <1003659191@qq.com>
2024-04-24 01:45:27 +0000

    feat: add api

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from openai_RAG_using_tavily import openai_rag,prompt
-from fastapi import FastAPI
 from typing import Union
 from pydantic import BaseModel
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -10,10 +10,14 @@ class Item(BaseModel):
     price: float
     is_offer: Union[bool, None] = None
 
-
-
 @app.get("/rag/")
 def get_answer(question: str):
     res=openai_rag(question)
     return res
 
+@app.post("/search_futures_news")
+async def search_futures_news(request: Request):
+    data = await request.json()
+    question = data.get("question")
+    res=openai_rag(question)
+    return { "response": res, "status": "success" }
