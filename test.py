f739363d4eb62e1133bef22041d11172c6ac0a93
Justin Ji <166482436+JiJiaqi01@users.noreply.github.com>
2024-04-26 16:10:44 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,26 @@
+from elasticsearch import Elasticsearch
+from langchain_elasticsearch import ElasticsearchStore
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings,OpenAI
+
+OPENAI_BASE_URL="https://api.chatanywhere.com.cn/v1"
+OPENAI_API_KEY="sk-AUnJk0FMih4MJp32GAyNzEQRi5KtVeiZlvRU6tROmaOAFvD9"
+OPENAI_MODEL="gpt-3.5-turbo-0125"
+OPENAI_EMBEDDING_MODEL="text-embedding-3-small"
+
+#create openai_embedding
+embeddings= OpenAIEmbeddings(openai_api_base=OPENAI_BASE_URL,
+                             openai_api_key=OPENAI_API_KEY,
+                             model=OPENAI_EMBEDDING_MODEL)
+
+#run this on virtual (域名和http格式不一致)
+vectorstore = ElasticsearchStore(
+    embedding=embeddings,
+    index_name="langchain-demo",
+    es_connection="jq.debian.typist.cc:9200"
+)
+
+question="what is biofuel"
+
+res=vectorstore.search(question,"mmr")
+
+print(res)
