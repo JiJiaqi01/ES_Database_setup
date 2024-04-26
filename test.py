60d9ab6b4e68516df4a8106aa1806ddf389d4f2a
Justin Ji <166482436+JiJiaqi01@users.noreply.github.com>
2024-04-26 17:41:38 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 from elasticsearch import Elasticsearch
 from langchain_elasticsearch import ElasticsearchStore
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings,OpenAI
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
 
 OPENAI_BASE_URL="https://api.chatanywhere.com.cn/v1"
 OPENAI_API_KEY="sk-AUnJk0FMih4MJp32GAyNzEQRi5KtVeiZlvRU6tROmaOAFvD9"
 OPENAI_MODEL="gpt-3.5-turbo-0125"
 OPENAI_EMBEDDING_MODEL="text-embedding-3-small"
-
+#es=Elasticsearch(hosts=["https://jq.debian.typist.cc:9200"])
 #create openai_embedding
 embeddings= OpenAIEmbeddings(openai_api_base=OPENAI_BASE_URL,
                              openai_api_key=OPENAI_API_KEY,
@@ -16,8 +18,27 @@ embeddings= OpenAIEmbeddings(openai_api_base=OPENAI_BASE_URL,
 vectorstore = ElasticsearchStore(
     embedding=embeddings,
     index_name="langchain-demo",
-    es_url="https://jq.debian.typist.cc:9200"
+    es_url="https://jq.debian.typist.cc:9200",
+    #es_connection=es
 )
+
+loader = TextLoader("./state_of_the_union.txt")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+
+db = ElasticsearchStore.from_documents(
+    docs,
+    embeddings,
+    es_url="http://localhost:9200",
+    index_name="test-basic",
+)
+
+db.client.indices.refresh(index="test-basic")
+query = "What did the president say about Ketanji Brown Jackson"
+results = db.similarity_search(query)
+print(results)
+
 
 question="what is biofuel"
 
