b9a2f81ff45f178613b5b81c2895c4ffebde0e3d
Justin Ji <166482436+JiJiaqi01@users.noreply.github.com>
2024-04-30 16:39:42 +0800

    Add files via upload

---
+++ Upload_to_es_database_oafa.py
@@ -0,0 +1,123 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+from langchain_openai import OpenAIEmbeddings
+from elasticsearch import Elasticsearch
+from langchain_elasticsearch import ElasticsearchStore
+from langchain_core.documents.base import Document
+#use log to find errors while running
+import logging
+logger=logging.getLogger()
+
+headers={
+    "User_Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+}
+
+OPENAI_BASE_URL="https://api.chatanywhere.com.cn/v1"
+OPENAI_API_KEY="sk-AUnJk0FMih4MJp32GAyNzEQRi5KtVeiZlvRU6tROmaOAFvD9"
+OPENAI_MODEL="gpt-3.5-turbo-0125"
+OPENAI_EMBEDDING_MODEL="text-embedding-3-small"
+
+
+#create openai_embedding
+embeddings= OpenAIEmbeddings(openai_api_base=OPENAI_BASE_URL,
+                             openai_api_key=OPENAI_API_KEY,
+                             model=OPENAI_EMBEDDING_MODEL)
+
+es=Elasticsearch(hosts=["http://jq.debian.typist.cc:9200"])
+#run this on virtual (域名和http格式不一致)
+vectorstore = ElasticsearchStore(
+    embedding=embeddings,
+    #index_name只有一个,数据库存的啥这里填啥
+    index_name="bio_database",
+    es_url="http://jq.debian.typist.cc:9200",
+    es_connection=es
+)
+
+
+
+
+#建立函数方便爬取不同网页
+def store_es(url,headers=headers):
+    #url 为对应网页
+    #返回页面源代码
+    response=requests.get(url=url,headers=headers)
+    html_text=response.text
+    #用soup筛选<p>获取文本
+    soup=BeautifulSoup(html_text,'html.parser')
+    ele=soup.select('p')
+    #用full_text来存储页面文本信息
+    full_text=""
+    #遍历response set去除<xxx> 保留文本
+    for element in ele[0:-1]:
+        full_text=full_text+element.get_text(separator="\n",strip=True)
+    
+    #将该得到的数据text,html,url导入es,并用openai embedding向量化
+    vectorstore.add_documents(documents=[Document(page_content=full_text,
+                                                  metadata={
+                                                      "url":url,
+                                                      "html":html_text,
+                                                      }
+                                                  )
+                                         ]
+                              )
+    
+#以上函数调用的时候输入网址就可以把网页文本存入es
+#下一步，获取网页地址
+xml_list=['https://climate.ec.europa.eu/sitemap.xml',
+          'https://agriculture.ec.europa.eu/sitemap.xml',
+          'https://new-european-bauhaus.europa.eu/sitemap.xml',
+          'https://economy-finance.ec.europa.eu/sitemap.xml',
+          'https://energy.ec.europa.eu/sitemap.xml',
+          'https://environment.ec.europa.eu/sitemap.xml',
+          'https://single-market-economy.ec.europa.eu/sitemap.xml',
+          'https://research-and-innovation.ec.europa.eu/sitemap.xml',
+          'https://commission.europa.eu/sitemap.xml',
+          'https://transport.ec.europa.eu/sitemap.xml',
+          'https://www.iata.org/sitemap.xml',
+          'https://ww2.arb.ca.gov/sitemap.xml?page=1',
+          'https://ww2.arb.ca.gov/sitemap.xml?page=2',
+          'https://biofuels-news.com/news-sitemap.xml',
+          'https://biofuels-news.com/news-sitemap2.xml',
+          'https://biofuels-news.com/news-sitemap3.xml',
+          'https://biofuels-news.com/news-sitemap4.xml',
+          'https://biofuels-news.com/news-sitemap5.xml',
+          'https://biofuels-news.com/news-sitemap6.xml',
+          'https://biofuels-news.com/news-sitemap7.xml',
+          'https://biofuels-news.com/news-sitemap8.xml',
+          'https://biofuels-news.com/news-sitemap9.xml'
+          ]
+
+sitemap_set=[]
+for xml in xml_list:
+    response=requests.get(url=xml,headers=headers)
+    html_text=response.text
+    #Use soup to take all the urls
+    soup=BeautifulSoup(html_text,'xml')
+    ele=soup.select('loc')
+    for element in ele:
+        #ele[x]都是str type
+        #去除<loc>,<\loc>
+        sitemap_set.append(element.get_text(separator="\n",strip=True))
+
+
+#建立一个log用于输出程序错误
+with open("log.txt","w") as f:
+    f.write("This is a log."+"\n"+"\n")
+
+index_count=-1
+#现在网页获取完毕，对每个网页调用store_es
+for url in sitemap_set:
+    #这一步用来确定哪个url出问题了,可通过index寻找
+    index_count=index_count+1
+    try:
+        store_es(url)
+    except Exception as e:
+        index=str(index_count)
+        logger.error(e)
+        with open("log.txt","a") as f:
+            f.write("index "+index+" error:"+"\n"+e+"\n")
+
+        
+    
