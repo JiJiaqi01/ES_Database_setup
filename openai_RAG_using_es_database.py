95fb80670974bb60bad9126ceb95cfbf1fd0334a
Justin Ji <166482436+JiJiaqi01@users.noreply.github.com>
2024-05-21 13:11:00 +0800

    Add time filter

--- openai_RAG_using_es_database.py
+++ openai_RAG_using_es_database.py
@@ -5,7 +5,7 @@ from langchain_core.prompts import PromptTemplate
 from datetime import datetime,timedelta,timezone
 from elasticsearch import Elasticsearch
 from langchain_elasticsearch import ElasticsearchStore
-from turn_into_structure import get_filter
+
 
 #running local using docker
 
@@ -67,6 +67,56 @@ The current date is {current_date} in format year/month/day.
 
 #generate the prompt
 prompt = PromptTemplate.from_template(template)
+
+#make a filter to get more precise information
+
+
+def get_filter(question):
+    #现在template只有三个keyword,后续可加入，但是由于是metadata进行filter,入库的时候估计还得添加额外内容
+    template = """You are here to help me to interpret a question and response with several key points:
+    1. What is the key information this question is asking for, response with format  "keyword: " following with a term not exceed five words
+    2. How many returns does this question ask for,response with format  "Limit: " following with a number
+    3. Is there any date information required in the question, such as "latest", "year 2022" if there is, response with the format "Time range: " and add the time condition
+    If the question ask for latest, recent, or any other words similar to this meaning, generate a date range from 30 days ago to now, the time now is {current_utc_time}
+    If anything else specific, calculate that date range using numbers of days from now.
+    In any case, your answer should follow the time format: '%Y-%m-%dT%H:%M:%SZ' to '%Y-%m-%dT%H:%M:%SZ' 
+
+    Remember:
+    Your answer should be in the following format, the string in the "" is what you must remain unchanged, and I will
+    notate what you should response using (your answer)
+
+    The format is:
+    "Keyword: " (your answer)
+    "Limit: " (your answer)
+    "Time range" (your answer)
+
+    question: {question}
+
+
+    """
+    #generate the prompt
+    prompt = PromptTemplate.from_template(template)
+    #use Open AI as our model
+    #此处直接使用openai中转点的api,如果后续有官方的api可调整变量
+    llm=ChatOpenAI(base_url='https://api.chatanywhere.com.cn/v1',api_key='sk-AUnJk0FMih4MJp32GAyNzEQRi5KtVeiZlvRU6tROmaOAFvD9', model='gpt-3.5-turbo-0125')
+
+
+    #create a llm_chain
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    current_utc=datetime.now(timezone.utc)
+    #use the chain to query a answer
+    answer=llm_chain.invoke(
+        {
+            "question": question,
+            "current_utc_time": current_utc
+        }
+    )
+
+
+    ans=answer['text']
+    return ans
+
+
 
 
 #this function is for generating answer using RAG. input is the user's question
